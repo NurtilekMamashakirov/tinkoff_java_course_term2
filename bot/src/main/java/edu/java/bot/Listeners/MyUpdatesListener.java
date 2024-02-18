3b9246f22e3d02b646c59946fa3b1ffec35946b4
NurtilekMamashakirov <nurtilekmamashakirov@gmail.com>
2024-02-19 00:15:36 +0300

    without tests4

--- bot/src/main/java/edu/java/bot/Listeners/MyUpdatesListener.java
+++ bot/src/main/java/edu/java/bot/Listeners/MyUpdatesListener.java
@@ -1,15 +1,32 @@
 package edu.java.bot.Listeners;
 
+import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import edu.java.bot.UpdateHandlers.HelpHandler;
+import edu.java.bot.UpdateHandlers.ListHandler;
+import edu.java.bot.UpdateHandlers.StartHandler;
+import edu.java.bot.UpdateHandlers.TrackHandler;
+import edu.java.bot.UpdateHandlers.UntrackHandler;
 import java.util.List;
 
 public class MyUpdatesListener implements UpdatesListener {
 
+    TelegramBot bot;
+
+    public MyUpdatesListener(TelegramBot bot) {
+        this.bot = bot;
+    }
+
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-
+            StartHandler startHandler = new StartHandler(bot, null, update);
+            HelpHandler helpHandler = new HelpHandler(bot, startHandler, update);
+            TrackHandler trackHandler = new TrackHandler(bot, helpHandler, update);
+            UntrackHandler untrackHandler = new UntrackHandler(bot, trackHandler, update);
+            ListHandler listHandler = new ListHandler(bot, untrackHandler, update);
+            listHandler.handle();
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
