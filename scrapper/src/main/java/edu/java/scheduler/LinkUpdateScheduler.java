999bff4b735d910109fbbe84c0fead4a714fd673
Мамашакиров Нуртилек Жанабидинович <nzmamashakirov@edu.hse.ru>
2024-02-26 14:31:24 +0300

    ready

---
+++ scrapper/src/main/java/edu/java/scheduler/LinkUpdateScheduler.java
@@ -0,0 +1,17 @@
+package edu.java.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@EnableScheduling
+@Component
+public class LinkUpdateScheduler {
+    @Scheduled(fixedDelayString = "#{@scheduler.interval}")
+    public void update() {
+        log.info("Updating...");
+    }
+
+}
