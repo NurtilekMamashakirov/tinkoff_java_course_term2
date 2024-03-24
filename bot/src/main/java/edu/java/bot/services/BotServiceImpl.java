9e1b94ca82445a41f62fafbb306ac3f1ddf4c856
Мамашакиров Нуртилек Жанабидинович <nzmamashakirov@edu.hse.ru>
2024-03-24 22:33:56 +0300

    hw started

--- bot/src/main/java/edu/java/bot/services/BotServiceImpl.java
+++ bot/src/main/java/edu/java/bot/services/BotServiceImpl.java
@@ -1,12 +1,22 @@
 package edu.java.bot.services;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
 import edu.java.bot.dto.request.LinkUpdate;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class BotServiceImpl implements BotService {
+    private static final String UPDATE_MESSAGE = "Поступило обновление по ссылке ";
+    private TelegramBot bot;
+
     @Override
     public void handleUpdates(LinkUpdate linkUpdate) {
-        //todo
+        for (Integer chatId : linkUpdate.tgChatIds()) {
+            SendMessage message = new SendMessage(linkUpdate.id(), UPDATE_MESSAGE + linkUpdate.url().toString());
+            bot.execute(message);
+        }
     }
 }
